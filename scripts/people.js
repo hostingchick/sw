a76240043b6bf19d748438b4f8ee3ac22a9f544e
jgrow <hostingchick@gmail.com>
2017-10-19 16:51:51 -0500

    added validation. updated to angular 1.6.4

--- scripts/people.js
+++ scripts/people.js
@@ -1,13 +1,17 @@
 var swInfo = angular.module('swInfo', []);
 swInfo.controller('people', function($scope,$http) {
+  $scope.regex = '^[a-zA-Z0-9._-]+$';
   $scope.searchPeople = function() {
-    var searchurl = "https://swapi.co/api/people?search="+ $scope.searchName;
-    $http({method: 'GET', url: searchurl}).
-    success(function(data,status,headers,config){
-      alert(JSON.stringify(data.results));
-    }).
-    error(function(data,status,headers,config){
-      alert(status);
-    });
+    var searchurl = "https://swapi.co/api/people?search="+ angular.uppercase($scope.searchName);
+    $http({
+      method: 'GET',
+      url: searchurl
+    }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        alert(JSON.stringify(response.data.results));
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        alert(response.statusText);
+      });
   }
 });
